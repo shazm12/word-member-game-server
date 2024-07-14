abda7b85b6413f17d59c4d5a494482b11a8f3225
shazm12 <shamik.bera2019@vitstudent.ac.in>
2024-07-15 00:46:47 +0530

    fix: change func return type to HTTPResponse; feat: Directly create instance of struct in json() fn;

--- src/controllers/word_controller.rs
+++ src/controllers/word_controller.rs
@@ -1,26 +1,23 @@
 use std::env;
 
-use actix_web::{web, HttpResponse};
-use mongodb::{bson::{bson, Bson}, Client, Collection};
+use actix_web::HttpResponse;
+use mongodb::Client;
 
-use crate::models::{success::Success, word::Word, error::Error};
+use crate::models::{error::Error, success::Success, word::Word};
 
-
-pub async fn post_word_doc(data: Word, mongo_client: &Client) -> () {
-    
+pub async fn post_word_doc(data: Word, mongo_client: &Client) -> HttpResponse {
     let db = env::var("DB_NAME").unwrap_or_else(|_| "".into());
     let collection = env::var("COLLECTION_NAME").unwrap_or_else(|_| "".into());
-    
+
     let collection = mongo_client.database(&db).collection(&collection);
     let result = collection.insert_one(data).await;
-    
-    let success_msg = Success {
-        message: "Word Doc Created!".to_string()
-    };
-    
-    match result {
-        Ok(_) => HttpResponse::Ok().json(&success_msg),
-        Err(err) => HttpResponse::InternalServerError().json(Error{ message: err.to_string() }),
-    };
 
+    match result {
+        Ok(_) => HttpResponse::Ok().json(Success {
+            message: "Word Doc Created!".to_string(),
+        }),
+        Err(err) => HttpResponse::InternalServerError().json(Error {
+            message: err.to_string(),
+        }),
+    }
 }
