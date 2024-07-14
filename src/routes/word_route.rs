bb822c0b507fdc664b4485389a503959a381c449
shazm12 <shamik.bera2019@vitstudent.ac.in>
2024-07-15 00:48:01 +0530

    fix: directly just return response from controller fn(post_word_doc);

--- src/routes/word_route.rs
+++ src/routes/word_route.rs
@@ -1,12 +1,12 @@
 use crate::models::word::Word;
 use crate::{controllers::word_controller::post_word_doc, AppState};
-use actix_web::{get, post, web, HttpResponse, Responder};
+use actix_web::{post, web, Responder};
 
 #[post("/create")]
 async fn handle_word(item: web::Json<Word>, app_state: web::Data<AppState>) -> impl Responder {
     let mongo_client = &app_state.mongo_client;
     let response = post_word_doc(item.into_inner(), &mongo_client).await;
-    HttpResponse::Ok().json(response)
+    response
 }
 
 pub fn init(cfg: &mut web::ServiceConfig) {
