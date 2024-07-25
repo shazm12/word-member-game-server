d1c1bb9d057213ba9d2441f2d7380dfb5408512f
shazm12 <shamik.bera2019@vitstudent.ac.in>
2024-07-26 00:35:17 +0530

    chore: created new controller handler function get_random_word_doc for /word/get GET endpoint;

--- src/controllers/word_controller.rs
+++ src/controllers/word_controller.rs
@@ -1,16 +1,20 @@
 use std::env;
-
-use actix_web::HttpResponse;
-use mongodb::Client;
-
-use crate::models::{error::Error, success::Success, word::Word};
+use actix_web::{ HttpResponse };
+use mongodb::{bson::doc, Client};
+use rand::Rng;
+use crate::models::{error::Error, success::Success, word::Word, word_array::WordArray};
+use futures_util::stream::TryStreamExt;
 
 pub async fn post_word_doc(data: Word, mongo_client: &Client) -> HttpResponse {
     let db = env::var("DB_NAME").unwrap_or_else(|_| "".into());
     let collection = env::var("COLLECTION_NAME").unwrap_or_else(|_| "".into());
-
     let collection = mongo_client.database(&db).collection(&collection);
-    let result = collection.insert_one(data).await;
+    let word_to_insert = Word {
+        name: data.name,
+        members: data.members,
+        random: Some(rand::thread_rng().gen_range(1..=100))
+    };
+    let result = collection.insert_one(word_to_insert).await;
 
     match result {
         Ok(_) => HttpResponse::Ok().json(Success {
@@ -20,4 +24,27 @@ pub async fn post_word_doc(data: Word, mongo_client: &Client) -> HttpResponse {
             message: err.to_string(),
         }),
     }
+}
+
+
+pub async fn get_random_word_doc(mongo_client: &Client) -> HttpResponse {
+    let db = env::var("DB_NAME").unwrap_or_else(|_| "".into());
+    let collection_name = env::var("COLLECTION_NAME").unwrap_or_else(|_| "".into());
+    let collection = mongo_client.database(&db).collection::<Word>(&collection_name);
+    let random_number = rand::thread_rng().gen_range(1..=100);
+    let query = doc! {"random": doc! {"$gt": random_number}};
+
+    let mut cursor = collection.find(query).await.expect("Failed to execute query");
+    let mut words: Vec<Word> = Vec::new();
+
+    // Iterate over the cursor and handle each document
+    while let Ok(Some(word)) = cursor.try_next().await {
+        words.push(word)
+    }
+
+    HttpResponse::Ok().json(WordArray{ 
+        data: words
+    })
+
+
 }
