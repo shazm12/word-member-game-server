f05ee28507cdbd2e402479fc239a1844fd1ca6c3
shazm12 <shamik.bera2019@vitstudent.ac.in>
2024-07-26 00:36:53 +0530

    chore: created new endpoint /word/get and configred as service;

--- src/routes/word_route.rs
+++ src/routes/word_route.rs
@@ -1,6 +1,7 @@
+use crate::controllers::word_controller::get_random_word_doc;
 use crate::models::word::Word;
 use crate::{controllers::word_controller::post_word_doc, AppState};
-use actix_web::{post, web, Responder};
+use actix_web::{get, post, web, Responder};
 
 #[post("/create")]
 async fn handle_word(item: web::Json<Word>, app_state: web::Data<AppState>) -> impl Responder {
@@ -9,6 +10,15 @@ async fn handle_word(item: web::Json<Word>, app_state: web::Data<AppState>) -> i
     response
 }
 
+#[get("/get")]
+async fn get_random_word(app_state: web::Data<AppState>) -> impl Responder {
+    let mongo_client = &app_state.mongo_client;
+    let response = get_random_word_doc(&mongo_client).await;
+    response
+}
+
 pub fn init(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::scope("/word").service(handle_word));
+    cfg.service(web::scope("/word")
+                    .service(handle_word)
+                    .service(get_random_word));
 }
